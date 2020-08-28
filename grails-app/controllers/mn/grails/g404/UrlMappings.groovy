8857a601ab9b27945cf89990092728e8fce3073e
Vladimir Orany <vladimir@orany.cz>
2020-08-28 09:19:11 +0200

    Initial commit

---
+++ grails-app/controllers/mn/grails/g404/UrlMappings.groovy
@@ -0,0 +1,16 @@
+package mn.grails.g404
+
+class UrlMappings {
+
+    static mappings = {
+        "/$controller/$action?/$id?(.$format)?"{
+            constraints {
+                // apply constraints here
+            }
+        }
+
+        "/"(view:"/index")
+        "500"(view:'/error')
+        "404"(view:'/notFound')
+    }
+}
---
+++ grails-app/init/mn/grails/g404/BootStrap.groovy
@@ -0,0 +1,9 @@
+package mn.grails.g404
+
+class BootStrap {
+
+    def init = { servletContext ->
+    }
+    def destroy = {
+    }
+}
